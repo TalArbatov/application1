adad4fb163d801c505bafc4339d8aee3f3310dc8
TalArbatov <talarbatov98@gmail.com>
2018-10-07 17:19:33 +0300

    localization in phone and portrait devices

--- src/components/Header/CustomDrawer/CustomDrawer.js
+++ src/components/Header/CustomDrawer/CustomDrawer.js
@@ -1,59 +1,58 @@
-import React from 'react';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import d from './CustomDrawer.css';
-import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InboxIcon from '@material-ui/icons/Code';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from "react";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import d from "./CustomDrawer.css";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from "@material-ui/icons/Code";
+import ListItemText from "@material-ui/core/ListItemText";
 import Scrollspy from "react-scrollspy";
+import { Typography } from "@material-ui/core";
+import LanguageButtons from './LanguageButtons/LanguageButtons';
 
 const CustomDrawer = props => {
   return (
     <SwipeableDrawer
-    disableRestoreFocus={true}
-      anchor='left'
+      disableRestoreFocus={true}
+      anchor="left"
       open={props.drawerOpen}
       onClose={props.toggleDrawer}
       onOpen={this.toggleDrawer}
-      style={{position:'fixed'}}
+      style={{ position: "fixed" }}
       className={d.drawer}
     >
-
       <div className={d.drawerHeader}>
         <IconButton onClick={props.toggleDrawer}>
           <ChevronLeftIcon />
+          <Typography>Menu</Typography>
         </IconButton>
       </div>
       <Divider />
       <div className={d.list}>
         <List>
-          
           <Scrollspy
             items={["about-me", "skills", "section-3"]}
             currentClassName="is-current"
           >
-              <ListItem button component="a" href="#skills">
-                <ListItemIcon>
-                  <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="My Skills" />
-              </ListItem>
+            <ListItem button component="a" href="#skills">
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="My Skills" />
+            </ListItem>
             <ListItem button component="a" href="#about-me">
               <ListItemIcon>
                 <InboxIcon />
               </ListItemIcon>
               <ListItemText primary="About Me" />
             </ListItem>
+            <LanguageButtons />
           </Scrollspy>
         </List>
-
       </div>
-
-
     </SwipeableDrawer>
   );
 };
---
+++ src/components/Header/CustomDrawer/LanguageButtons/LanguageButtons.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { connect } from "react-redux";
+import { withNamespaces, NamespacesConsumer, Trans } from "react-i18next";
+import d from "./LanguageButtons.css";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from "@material-ui/icons/Code";
+import ListItemText from "@material-ui/core/ListItemText";
+import Scrollspy from "react-scrollspy";
+import { Typography } from "@material-ui/core";
+import * as actions from "../../../../store/actionTypes";
+
+class LanguageButtons extends React.Component {
+
+    languageHandler = lang => {
+        console.log("starting redux for " + lang);
+        this.props.toggleLanguage(lang);
+      };
+
+  render() {
+    const { t, i18n } = this.props;
+
+    const changeLanguage = lng => {
+      i18n.changeLanguage(lng);
+    };
+    return [
+      <ListItem  onClick={() => {
+        changeLanguage("he");
+        this.languageHandler("he");
+      }}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="עברית" />
+      </ListItem>,
+        <ListItem  onClick={() => {
+            changeLanguage("en");
+            this.languageHandler("en");
+          }}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="English" />
+          </ListItem>
+    ];
+  }
+}
+
+const mapStateToPros = state => {
+  return {
+    rootReducer: state
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleLanguage: lang => {
+      return dispatch({
+        type: actions.TOGGLE_LANGUAGE,
+        payload: {
+          language: lang
+        }
+      });
+    }
+  };
+};
+
+export default connect(
+  mapStateToPros,
+  mapDispatchToProps
+)(withNamespaces("translation")(LanguageButtons));
